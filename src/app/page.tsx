3098e7bdbaefbfb5dc0e0d3d59979e5bc069caf7
Cristian Garcia <cristiangarcianastar21@gmail.com>
2024-06-03 14:08:54 -0500

    Add: Convex query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,12 @@ import {
   SignedIn,
   SignedOut,
 } from "@clerk/nextjs";
-import { useMutation } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+
+  const files = useQuery(api.files.getFiles)
   const cratedFile = useMutation(api.files.createFile)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -24,6 +26,10 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      {files?.map((file)=>{
+        return <div key={file.id}>{file.name}</div>
+      })}
 
       <Button onClick={()=>{
         cratedFile({
